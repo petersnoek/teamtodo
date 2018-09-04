13c3dba8934d00d17c18e870ee5e055ec7bff0ca
Dennis van Blanken <99040363@mydavinci.nl>
2018-09-04 09:24:20 +0200

    Laravel + layout adminLTE

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::register')
+
+@section('title', 'Team ToDo - login')
+
+@section('css')
+
+    <link rel="stylesheet" href="/css/admin_custom.css">
+
+@stop
+
+@section('js')
+
+@stop
---
+++ resources/views/login.blade.php
@@ -0,0 +1,37 @@
+@extends('adminlte::login')
+
+
+
+@section('title', 'Team ToDo - login')
+
+
+
+@section('content_header')
+
+    <h1>Dashboard</h1>
+
+@stop
+
+
+
+@section('content')
+
+    <p>Welcome to this beautiful admin panel - HDTuto.com.</p>
+
+@stop
+
+
+
+@section('css')
+
+    <link rel="stylesheet" href="/css/admin_custom.css">
+
+@stop
+
+
+
+@section('js')
+
+    <script> console.log('Hi!'); </script>
+
+@stop
