13c905446a0529418c5de97a3ed66dd1b5939069
Thomas van Gaalen <thomasvangaalen@icloud.com>
2018-10-10 16:11:54 +0200

    Name navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,10 +26,10 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    TeamTodo
                 </a>
                 <a class="nav-item" href="/create/todo">
-                    Todo's
+                    Create todo
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
