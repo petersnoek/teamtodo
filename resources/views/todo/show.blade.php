0b125ddd78b4990a3c1d775b8685f0ab7811b26d
Thomas van Gaalen <thomasvangaalen@icloud.com>
2018-11-02 16:17:01 +0100

    Edit task modal fix

--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -43,35 +43,37 @@
 <hr>
 
 <div class="row justify-content-center">
-    <div style="margin-bottom: 20px;" class="col-md-8">
-        <div class="card">
-            <div class="card-header"><p style="margin: 7px 0;float: left;">Add user</p>
-                <form style="float: right;" method="post" action="{{url('store/todoUser')}}">
-                <form style="float: right;" method="post" action="{{url('store/todo/addUser')}}">
-                    @csrf
-                    <input hidden type="text" name="todo_id" value="{{ $todo->id }}">
-                    <input style="width: 200px;display: inline-block;" type="text" class="form-control" name="user" placeholder="Add user...">
-                    <button style="display: inline-block;" type="submit" class="btn btn-success">Submit</button>
-                </form>
+        <div style="margin-bottom: 20px;" class="col-md-8">
+            <div class="card">
+                <div class="card-header"><p style="margin: 7px 0;float: left;">Add user</p>
+                    {{--<form style="float: right;" method="post" action="{{url('store/todoUser')}}">--}}
+                    <form style="float: right;" method="post" action="{{url('store/todo/addUser')}}">
+                        @csrf
+                        <input hidden type="text" name="todo_id" value="{{ $todo->id }}">
+                        <input style="width: 200px;display: inline-block;" type="text" class="form-control" name="user" placeholder="Add user...">
+                        <button style="display: inline-block;" type="submit" class="btn btn-success">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">Add task</div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add task</div>
 
-            <div class="card-body">
-                <form method="post" action="{{url('store/todo/task')}}">
-                    @csrf
-                    <input hidden type="text" name="todo_id" value="{{ $todo->id }}">
+                <div class="card-body">
+                    <form method="post" action="{{url('store/todo/task')}}">
+                        @csrf
+                        <input hidden type="text" name="todo_id" value="{{ $todo->id }}">
 
-                    <div class="form-group col-md-4">
-                        <label for="Name">Task</label>
-                        <input type="text" class="form-control" name="content" placeholder="Add task...">
-                    </div>
-                    <div class="form-group col-md-4" style="margin-top:60px">
-                        <button type="submit" class="btn btn-success">Submit</button>
-                    </div>
-                </form>
+                        <div class="form-group col-md-4">
+                            <label for="Name">Task</label>
+                            <input type="text" class="form-control" name="content" placeholder="Add task...">
+                        </div>
+                        <div class="form-group col-md-4" style="margin-top:60px">
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
