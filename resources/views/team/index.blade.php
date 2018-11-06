327056de6988388e6e8852b2c337f4ab918e882f
Thomas van Gaalen <thomasvangaalen@icloud.com>
2018-11-06 09:32:09 +0100

    Messcodefix

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -7,12 +7,6 @@
         <div class="d-flex flex-row p-auto">
 
         @foreach($teams as $team)
-            {{--{{ 'team info' }}--}}
-            {{--{{ $team->id }}--}}
-            {{--{{ $team->name }} <br>--}}
-
-            {{--{{ var_dump($user) }}--}}
-
             @foreach($team->users as $user)
                 @if($user->pivot->user_id == $userId)
                         <div class="card border-secondary mb-3" style="max-width: 18rem;">
@@ -21,20 +15,7 @@
                                 <a href="/team/{{$team->id}}" class="card-title">Go to {{ $team->name }}</a>
                             </div>
                         </div>
-                    {{--<a href="/team/{{$team->id}}">{{$team->name}}</a>--}}
                 @endif
-
-                {{--<p>user info</p>--}}
-                {{--{{ $user->id }}--}}
-                {{--{{ $user->name }}--}}
-                {{--{{ $user->email }}--}}
-                {{--<p>team_id</p>--}}
-                {{--<p>pivot team id</p>--}}
-                {{--{{ var_dump($user->pivot->user_id) }}--}}
-                {{--{{ $user->pivot->team_id }} <br>--}}
-                {{--<p>user_id</p>--}}
-                {{--<p>pivot user id</p>--}}
-                {{--{{ $user->pivot->user_id }} <br>--}}
             @endforeach
         @endforeach
             </div>
