dfabd5006fc769c0e7b1306400b046b8c9680e27
Thomas van Gaalen <thomasvangaalen@icloud.com>
2018-11-06 12:29:33 +0100

    Team profile

--- resources/views/team/show.blade.php
+++ resources/views/team/show.blade.php
@@ -2,10 +2,26 @@
 
 @section('content')
 
-<p>hello</p>
+    <h1>{{ $team->name }}</h1>
 
-    <h1>{{$team->name }}</h1>
+    <hr>
 
-    {{dd($team)}}
+
+    @foreach($team->users as $user)
+        <ul>
+            <li>{{ $user->name }}</li>
+        </ul>
+    @endforeach
+
+    <div class="row">
+        <div class="col-sm-8">
+
+        </div>
+        <div class="col-sm-4">
+
+        </div>
+    </div>
+
+
 
 @endsection
