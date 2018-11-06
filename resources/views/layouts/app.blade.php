12df55e5bb8263676d4a370f7c4f10ecafa1ffac
Thomas van Gaalen <thomasvangaalen@icloud.com>
2018-11-06 19:35:31 +0100

    Style fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    @yield('style')
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
--- resources/views/team/show.blade.php
+++ resources/views/team/show.blade.php
@@ -5,18 +5,9 @@
     <h1>{{ $team->name }}</h1>
 
     <hr>
-    {{--{{ dd($noTeam) }}--}}
     <div class="row">
         <div class="col-sm-8">
-            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
 
-
-
-            <ul id="myUl" style="width: 300px; height: 100px; overflow: auto; list-style: none">
-                @foreach($noTeam as $user)
-                    <li><a href="/add/user/team/{{$team->id}}/{{$user->id}}">{{ $user->name }}</a></li>
-                @endforeach
-            </ul>
         </div>
         <div class="col-sm-4">
             <h2>members</h2>
@@ -26,11 +17,23 @@
                             <p> {{$user->name}} </p> <a href="/delete/team/{{ $team->id }}/{{ $user->id }}"><i style="float: right">x</i></a>
                             <p> {{ $user->email }}</p>
                             <i> member since {{ $user->pivot->created_at }}</i>
-                            <p> Team id; {{ $user->pivot->team_id  }}</p>
-                            <p> User id; {{ $user->pivot->user_id  }}</p>
                         </div>
                     </div>
             @endforeach
+            <h2>Invite users</h2>
+
+            <hr>
+
+            <div class="col">
+                    <input type="text" class="form-control" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+
+                <ul  id="myUl" style="width: 300px; height: 150px; overflow: auto; list-style: none">
+                    @foreach($noTeam as $user)
+                        <li><a class="list-group-item" href="/add/user/team/{{$team->id}}/{{$user->id}}">{{ $user->name }}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+
         </div>
     </div>
 
